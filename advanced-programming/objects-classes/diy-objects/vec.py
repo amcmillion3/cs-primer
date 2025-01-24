07e11ab69a05949cf686ba8e86bc05d6c13697bf
Andrew McMillion <amcmillion3@gmail.com>
2025-01-23 20:20:59 -0600

    DIY Objects.

--- advanced-programming/objects-classes/diy-objects/vec.py
+++ advanced-programming/objects-classes/diy-objects/vec.py
@@ -1,43 +1,33 @@
+def __str__(self):
+    return f'<{self["x"]}, {self["y"]}>'
 
-class Vec(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def magnitude(self):
+    """Return the magnitude of the vector"""
+    return (self["x"]**2 + self["y"]**2)**0.5
 
-    def __str__(self):
-        return f'<{self.x}, {self.y}>'
+def mul(self, s):
+    """Scalar multiplication"""
+    return {"x": s * self["x"], "y": s * self["y"]}
 
-    def magnitude(self):
-        """Return the magnitude of the vector"""
-        return (self.x**2 + self.y**2)**0.5
+def add(self, v2):
+    return {"x": self["x"] + v2["x"], "y": self["y"] + v2["y"]}
 
-    def __mul__(self, s):
-        """Scalar multiplication"""
-        return Vec(s * self.x, s * self.y)
+def sub(self, v2):
+    return {"x": self["x"] - v2["x"], "y": self["y"] - v2["y"]}
 
-    def __eq__(self, v2):
-        return self.x == v2.x and self.y == v2.y
-
-    def __add__(self, v2):
-        return Vec(self.x + v2.x, self.y + v2.y)
-
-    def __sub__(self, v2):
-        return Vec(self.x - v2.x, self.y - v2.y)
-
-    def __neg__(self):
-        return Vec(-self.x, -self.y)
-
+def neg(self):
+    return {"x": -self["x"], "y": -self["y"]}
 
 if __name__ == '__main__':
-    v1 = Vec(3, 2)
-    v2 = Vec(1, 1)
-    v3 = Vec(3, 4)
+    v1 = {"x": 3, "y": 2}
+    v2 = {"x": 1, "y": 1}
+    v3 = {"x": 3, "y": 4}
 
-    assert v2.magnitude() == 2**0.5
-    assert v3.magnitude() == 5
-    assert v1 * 2 == Vec(6, 4)
-    assert v1 + v2 == Vec(4, 3)
-    assert v1 - v2 == Vec(2, 1)
-    assert -v1 == Vec(-3, -2)
+    assert magnitude(v2) == 2**0.5
+    assert magnitude(v3) == 5
+    assert mul(v1, 2) == {"x": 6, "y": 4}
+    assert add(v1, v2) == {"x": 4, "y": 3}
+    assert sub(v1, v2) == {"x": 2, "y": 1}
+    assert neg(v1) == {"x": -3, "y": -2}
 
     print('ok')
